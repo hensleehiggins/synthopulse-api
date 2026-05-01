38fa207b011da4df79503964c6af0ca2f1ff9fb0
hensleehiggins <henslee.higgins@gmail.com>
2026-05-01 07:06:52 -0400

    Update latest-brief.js

--- api/latest-brief.js
+++ api/latest-brief.js
@@ -1,6 +1,13 @@
 const Airtable = require("airtable");
 
 export default async function handler(req, res) {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+res.setHeader("Access-Control-Allow-Methods", "GET, OPTIONS");
+res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+
+if (req.method === "OPTIONS") {
+  return res.status(200).end();
+}
   const base = new Airtable({ apiKey: process.env.AIRTABLE_PAT })
     .base(process.env.AIRTABLE_BASE_ID);
 
