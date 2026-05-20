f72739bb609c5162b702138a9c6b0e6961079226
hensleehiggins <henslee.higgins@gmail.com>
2026-05-20 01:17:58 -0400

    Implement CORS support in API handler
    Add CORS headers to the API response

--- api/tripleseat-leads-board.js
+++ api/tripleseat-leads-board.js
@@ -446,6 +446,14 @@ async function fetchTripleseatLeads() {
 }
 
 module.exports = async function handler(req, res) {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET, OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+
+  if (req.method === "OPTIONS") {
+    return res.status(200).end();
+  }
+
   try {
     const fetched = await fetchTripleseatLeads();
 
